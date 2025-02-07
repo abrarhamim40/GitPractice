f46d01759ed1871f56af0831f7bcf8ba0ed257cc
abrarhamim40 <abrarhamim4044j@gmail.com>
2025-02-07 12:31:45 +0600

    New Text Add

--- GitPractice/GitPractice/ContentView.swift
+++ GitPractice/GitPractice/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "plus")
+                .font(.system(size: 72).weight(.regular))
+                .frame(width: 72, height: 72)
+            
+            Text("Plus")
+                .font(.system(size: 32).bold())
+                .frame(width: 200, height: 200)
         }
         .padding()
     }
